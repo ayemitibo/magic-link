6a9b316e3624162b51a00e56da9083bc6a703783
Ayemitibo Abiodun <ayemitiboabiodun02@gmail.com>
2021-12-09 13:34:04 +0100

    chore: added magic link

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script>
+export default {
+  created(){
+     const plugin = document.createElement("script");
+    plugin.setAttribute(
+      "src",
+      "https://auth.magic.link/pnp/callback"
+    );
+    plugin.setAttribute(
+      "data-magic-publishable-api-key",
+      "pk_live_2C892575A2D828C6"
+    );
+    plugin.async = true;
+    document.head.appendChild(plugin);
+  },
+  mounted(){
+    window.addEventListener('@magic/ready', (event) => {
+    const { magic, idToken, userMetadata, oauth } = event.detail;
+    console.log(magic,idToken,userMetadata,oauth)
+    // ...
+  });
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+  name: 'App',
+  mounted(){
+     const plugin = document.createElement("script");
+    plugin.setAttribute(
+      "src",
+      "https://auth.magic.link/pnp/login"
+    );
+    plugin.setAttribute(
+      "data-magic-publishable-api-key",
+      "pk_live_2C892575A2D828C6"
+    );
+    plugin.setAttribute(
+      "data-redirect-uri",
+      "/dashboard"
+    );
+    plugin.async = true;
+    document.head.appendChild(plugin);
+  }
+}
+</script>
